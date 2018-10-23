e527bf7ad6166bc865d9374072b0267b77140e8a
daihu <2502427410@qq.com>
2018-10-23 17:10:23 +0800

    弹球

--- src/bounce/BallComponent.java
+++ src/bounce/BallComponent.java
@@ -1,4 +1,36 @@
 package bounce;
 
-public class BallComponent {
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Anderson.Dai
+ * @date 10/23/2018
+ */
+public class BallComponent extends JPanel {
+    private static final int DEFAULT_WIDTH = 450;
+    private static final int DEFAULT_HEIGHT = 350;
+
+    private List<Ball> balls = new ArrayList<>();
+
+    void add(Ball b) {
+        balls.add(b);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        // erase background
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        for (Ball b : balls) {
+            g2.fill(b.getShapeO());
+        }
+    }
+
+    @Override
+    public Dimension getPreferredSize(){
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
 }
--- src/bounce/Bounce.java
+++ src/bounce/Bounce.java
@@ -1,4 +1,55 @@
 package bounce;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+/**
+ * @author Anderson.Dai
+ * @date 10/23/2018
+ */
 public class Bounce {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            JFrame frame = new BounceFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}
+
+class BounceFrame extends JFrame {
+    private static final int STEPS = 1000;
+    private static final int DELAY = 3;
+    private BallComponent comp;
+
+    BounceFrame() {
+        setTitle("Bounce");
+        comp = new BallComponent();
+        add(comp, BorderLayout.CENTER);
+        JPanel buttonPanel = new JPanel();
+        addButton(buttonPanel, "Start", event -> addBall());
+        addButton(buttonPanel, "Close", event -> System.exit(0));
+        add(buttonPanel, BorderLayout.SOUTH);
+        pack();
+    }
+
+    private void addButton(Container c, String title, ActionListener listener) {
+        JButton button = new JButton(title);
+        c.add(button);
+        button.addActionListener(listener);
+    }
+
+    private void addBall() {
+        try {
+            Ball ball = new Ball();
+            comp.add(ball);
+            for (int i = 1; i <= STEPS; i++) {
+                ball.move(comp.getBounds());
+                comp.paint(comp.getGraphics());
+                Thread.sleep(DELAY);
+            }
+        } catch (InterruptedException e) {
+        }
+    }
 }
